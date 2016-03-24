929738268f1b84f2f836626128604d0c3c617c12
JV <juanvanni@icloud.com>
2016-03-23 21:29:21 -0300

    added min version of dist files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ import cssnano from 'cssnano';
 
 const $ = gulpLoadPlugins();
 
-gulp.task('sass', () => {
+gulp.task('styles', () => {
   return gulp.src('src/sass/*.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
@@ -16,34 +16,33 @@ gulp.task('sass', () => {
       outputStyle: 'expanded',
       includePaths: ['.']
     }).on('error', $.sass.logError))
+    .pipe($.postcss([
+      autoprefixer({browsers: ['> 1%']})
+    ]))
     .pipe($.sourcemaps.write())
     .pipe($.size({title: 'css'}))
-    .pipe(gulp.dest('.tmp'))
+    .pipe(gulp.dest('dist'))
 });
 
-gulp.task('compileCSS',['sass'], () => {
-  return gulp.src('.tmp/*.css')
+gulp.task('minify',['styles'], () => {
+  return gulp.src('dist/*.css')
     .pipe($.plumber())
-    .pipe($.postcss([
-      autoprefixer({browsers: ['last 5 versions']})
-    ]))
     .pipe($.postcss([
       cssnano()
     ]))
+    .pipe($.rename({
+      suffix: ".min"
+    }))
     .pipe($.size({title: 'css.min'}))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('watch', ['sass'], () => {
-  gulp.watch('src/styles/**/*.scss', ['sass']);
+gulp.task('watch', ['styles'], () => {
+  gulp.watch('src/styles/**/*.scss', ['styles']);
 });
 
-gulp.task('clean', del.bind(null, ['.tmp','dist']));
+gulp.task('clean', del.bind(null, ['dist']));
 
-gulp.task('default', ['compileCSS'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
-
-gulp.task('dist', ['clean'], () => {
-  gulp.start('default');
+gulp.task('default', ['clean'], () => {
+  gulp.start('minify');
 });
